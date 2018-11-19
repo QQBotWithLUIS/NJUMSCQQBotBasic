c453f83dec61fe65625319e3483649dee057dcb8
SimpleLin <171250015@smail.nju.edu.cn>
2018-11-19 19:06:50 +0800

    qna with bug
    返回的结果中文是“\u..."的形式，可能需要转码，已尝试过整体使用.decode("...")但失败，可能需要单独转码/获取方式不对，目前代码来自 https://docs.microsoft.com/en-us/azure/cognitive-services/qnamaker/quickstarts/python

---
+++ submit2QnAmakerAndGetAnswer.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json, time
+
+# NOTE: Replace this with a valid host name.
+host = "se-service-robot.azurewebsites.net"
+
+# NOTE: Replace this with a valid endpoint key.
+# This is not your subscription key.
+# To get your endpoint keys, call the GET /endpointkeys method.
+endpoint_key = "31d46bf3-7424-4f6f-812e-ba0b536c503c"
+
+# NOTE: Replace this with a valid knowledge base ID.
+# Make sure you have published the knowledge base with the
+# POST /knowledgebases/{knowledge base ID} method.
+kb = "d4e8a182-75a4-4fd5-a07f-3709a969d63a"
+
+method = "/qnamaker/knowledgebases/" + kb + "/generateAnswer"
+
+question = {
+    'question': '你是谁呀',
+    'top': 1
+}
+
+def pretty_print (content):
+# Note: We convert content to and from an object so we can pretty-print it.
+    return json.dumps(json.loads(content), indent=4)
+
+def get_answers (path, content):
+    print ('Calling ' + host + path + '.')
+    headers = {
+        'Authorization': 'EndpointKey ' + endpoint_key,
+        'Content-Type': 'application/json',
+        'Content-Length': len (content)
+    }
+    conn = http.client.HTTPSConnection(host)
+    conn.request ("POST", path, content, headers)
+    response = conn.getresponse ()
+    return response.read()
+
+
+# Convert the request to a string.
+content = json.dumps(question)
+result = get_answers (method, content)
+print (result["answers"])
