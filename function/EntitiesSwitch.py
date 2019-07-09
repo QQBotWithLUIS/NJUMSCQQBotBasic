c95622f9d5f3f18a0ddeb73f9249be67c94521d6
HLin Xu <32236576+SimpleLonely@users.noreply.github.com>
2019-07-09 12:28:16 +0800

    weather

--- function/EntitiesSwitch.py
+++ function/EntitiesSwitch.py
@@ -1,10 +1,11 @@
 import random
+from function.weather import query_weather
 
 def entities_match(data):
     """函数体，采用switch的方式对传入的data进行分析，识别其中的实体
        如果其中有和列表中实体相同的实体，则返回索引值
        否则，返回-1作为异常标志"""
-    entities_list = ["活动部","校运会","技术部","宣传部"]
+    entities_list = ["活动部", "校运会", "技术部", "宣传部"]
     # 可以在entities中增加足够多的实体，来完成不同的实体识别
     for entity in data:
         if entity.as_dict()["entity"] in entities_list:
@@ -22,49 +23,77 @@ def entities_ans(index):
     ]
     return ans_list[index]
 
-def entities_module_match(data):
+#def weather_query_match(data):
+
+
+def entities_module_match(full_data):
     """前面的函数虽然实现了一定的功能，但是在同一大类的实体识别的时候，运维十分的繁琐
        或许可以增加一个以大类进行识别的方式"""
 
-    ask_weather = ["天气","温度"]
-    technology_team = ["技术部","技术队","技术家园"]
-    activity_team = ["活动部","活动队"]
-    #2018.12.16 预备更新，为即将到来的天气预报做准备，将其仍然耦合在原有的结构中
+    data = full_data.entities
+    ask_weather = ["天气", "温度"]
+    technology_team = ["技术部", "技术队", "技术家园"]
+    activity_team = ["活动部", "活动队"]
+    # 2018.12.16 预备更新，为即将到来的天气预报做准备，将其仍然耦合在原有的结构中
 
-    entities_module_list = [ask_weather, technology_team, activity_team ]
-    #2018.12.16 注意天气的问询就成为了特殊的0号索引，在下一部分的函数调用的时候需要小心这一点
+    entities_module_list = [ask_weather, technology_team, activity_team]
+    # 2018.12.16 注意天气的问询就成为了特殊的0号索引，在下一部分的函数调用的时候需要小心这一点
+
+    if full_data.top_scoring_intent.intent  == "天气.查询天气":
+        return 0
+    else:
+        # entity_module_find = 0
+        power = 0.0000000
+        # ret_list = ["NULL"]
+        ret_index = -1
+
+        for entity_module in entities_module_list:
+            for entity in data:
+                if entity.as_dict()["entity"] in entity_module:
+                    # print(float(entity.as_dict()["score"]))
+                    # return entities_module_list.index(entity_module) #如果是直接返回的话，忽略了权重这一个指标
+                    if float(entity.as_dict()["score"]) > power:
+                        ret_index = entities_module_list.index(entity_module)
+        return ret_index
 
 
-    """
-    曾用代码：
-    for entity in data:
-        if entity.as_dict()["entity"] in entities_list:
-            return entities_list.index(entity.as_dict()["entity"])
-    """
-    # entity_module_find = 0
-    power = 0.0000000
-    # ret_list = ["NULL"]
-    ret_index = -1
+def get_weather_ans(entities):
+    if len(entities) == 0:
+        return "暂时没有查询到您想要的天气"
+    elif len(entities) >1:
+        typenum = 0
+        city_name = ""
+        for entity in entities:
+            if entity.type == "城市":
+                typenum+=1
+                city_name = entity
+        if typenum > 1 or typenum == 0:
+            return "每次请只查询一座城市哟~"
+        else:
+            ans = query_weather(city_name)
+            return ans
+    else:
+        entity = entities[0]
+        city_name = entity.entity
+        if entity.type != "城市":
+            return "请输入城市的名称"
+        else:
+            ans = query_weather(city_name)
+            return ans
+            
 
 
-    for entity_module in entities_module_list:
-        for entity in data:
-            if entity.as_dict()["entity"] in entity_module:
-                # print(float(entity.as_dict()["score"]))
-                # return entities_module_list.index(entity_module) #如果是直接返回的话，忽略了权重这一个指标
-                if float(entity.as_dict()["score"]) > power:
-                    ret_index = entities_module_list.index(entity_module)
-    return ret_index
+
 
 
 def entities_module_ans(index):
     """与上面的模式化匹配实体相对应，模式化的回应"""
     if index == 0:
-        #此时是特殊的天气查询
-        return "天气预报"#天气查询的函数
+        # 此时是特殊的天气查询
+        return "天气预报"  # 天气查询的函数
 
     else:
-        index -= 1#此时为了匹配固定位置上的回复函数，采用这样的方法进行重新定位
+        index -= 1  # 此时为了匹配固定位置上的回复函数，采用这样的方法进行重新定位
 
     technology_team_ans = [
         "技术部都是帅哥靓女",
