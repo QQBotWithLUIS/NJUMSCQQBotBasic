7b793e01960392bc12580c49c9f8073001d3f537
Nosolution <32201220+Nosolution@users.noreply.github.com>
2019-07-09 10:53:37 +0800

    feat: 对输出的群组信息进行格式化，而不是输出json; 发送指令帮助时判断请求者身份，发送符合身份的命令帮助列表

--- plugins/base.py
+++ plugins/base.py
@@ -10,11 +10,6 @@ prompt = '若想使用该指令，可输入\'<指令名> -h/--help\'获知用法
 prompt_super = '若想使用该指令，可输入\'<指令名> -h/--help\'获知用法(本指令管理员可用)'
 
 
-# @on_command('inform', permission=perm.SUPERUSER)
-# async def inform(session: CommandSession):
-#     await session.send(session.get_optional('message') or session.current_arg)
-
-
 @on_command('天气', only_to_me=True)
 async def weather(session: CommandSession):
     info = (session.get_optional('message') or session.current_arg).split(' ')
@@ -27,7 +22,7 @@ async def weather(session: CommandSession):
         await session.finish("非常抱歉，查询失败了呢")
 
 
-@on_command('私信', only_to_me=True, shell_like=True)
+@on_command('私信', only_to_me=True, shell_like=True, permission=perm.PRIVATE_FRIEND)
 async def private_msg(session: CommandSession):
     parser = ArgumentParser(session=session,
                             prompt=prompt,
@@ -71,7 +66,7 @@ async def private_msg(session: CommandSession):
             await session.finish('发送失败')
 
 
-@on_command('发送群消息', only_to_me=True, shell_like=True)
+@on_command('发送群消息', only_to_me=True, shell_like=True, permission=perm.PRIVATE_FRIEND)
 async def group_msg(session: CommandSession):
     parser = ArgumentParser(session=session,
                             prompt=prompt,
@@ -118,9 +113,9 @@ async def group_msg(session: CommandSession):
 async def get_friend_list(session: CommandSession):
     bot = get_bot()
     try:
-        list = await bot._get_friend_list()
-        await session.finish(str(list))
-    except ActionFailed:
+        l = await bot._get_friend_list()
+        await session.finish(str(l))
+    except ActionFailed as e:
         await session.finish('抱歉，查询失败')
 
 
@@ -128,8 +123,8 @@ async def get_friend_list(session: CommandSession):
 async def get_group_list(session: CommandSession):
     bot = get_bot()
     try:
-        list = await bot.get_group_list()
-        await session.finish(str(list))
+        l = await bot.get_group_list()
+        await session.finish('\n'.join(format_group_list(l)))
     except ActionFailed:
         await session.finish('抱歉，查询失败')
 
@@ -144,7 +139,7 @@ async def get_group_info(session: CommandSession):
     bot = get_bot()
     try:
         info = await bot._get_group_info(group_id=int(args.target))
-        await session.finish(str(info))
+        await session.finish(format_group_info(info))
     except ActionFailed:
         session.finish('抱歉，查询失败')
 
@@ -192,14 +187,61 @@ async def get_group_member_info(session: CommandSession):
             session.finish('抱歉，查询失败')
 
 
-@on_command("command_help", only_to_me=True)
+@on_command("command_help", only_to_me=True, aliases=['帮助', '指令帮助'])
 async def get_command_help(session: CommandSession):
-    h0 = " 指令执行途中可发送'取消'停止执行 "
+    is_superuser = await perm.check_permission(session.bot, session.ctx, perm.SUPERUSER)
+    h0 = "本人工智能提供以下可使用的指令:"
+    he = " 指令执行过程中可发送'取消'停止执行 "
     h1 = "1. \'私信\' --to/-t <发送对象id> --msg/-m <消息>"
     h2 = "2. \'发送群消息\' --to/-t <发送群组id> --msg/-m <消息>"
-    h3 = "3. \'获取本号好友列表\'"
-    h4 = "4. \'获取本号群列表\'"
-    h5 = "5. \'获取群资料\' --target/-t <目标群组id>"
-    h6 = "6. \'获取群成员信息\' --group_id/-g <目标群组号> --user_id/-u <目标成员QQ号>"
-    h7 = "7. \'天气\' <城市名> (<县名>)"
-    await session.send('\n'.join([h1, h2, h3, h4, h5, h6, h7, h0]))
+    h3 = "3. \'天气\' <城市名> (<县名>)"
+    h4 = "4. \'获取本号好友列表\'(不稳定)"
+    h5 = "5. \'获取本号群列表\'"
+    h6 = "6. \'获取群资料\' --target/-t <目标群组id>"
+    h7 = "7. \'获取群成员信息\' --group_id/-g <目标群组号> --user_id/-u <目标成员QQ号>"
+    # 根据权限发送相应的帮助信息
+    if is_superuser:
+        await session.send('\n'.join([h0, h1, h2, h3, h4, h5, h6, h7, he]))
+    else:
+        await session.send('\n'.join([h0, h1, h2, h3, he]))
+
+
+# @on_command("test", only_to_me=True)
+# async def test_command(session: CommandSession):
+#     res = await get_friend_numbers(session.bot)
+#     await session.send(str(res))
+
+
+def format_friend_list(friend_group_list: list):
+    res = []
+
+
+def format_group_list(group_list: list):
+    res = list(map(lambda x: '群号: ' + str(x['group_id']) + ', 群名称: ' + str(x['group_name']), group_list))
+    return res
+
+
+def format_group_info(group_info: dict):
+    admins = group_info['admins']
+    admins_formatted = list(
+        map(lambda x: '昵称: ' + x['nickname'] + ', 身份: ' + x['role'] + ', QQ号: ' + str(x['user_id']), admins))
+
+    res = '群号: {}\n群名称: {}\n管理员数目: {}\n管理员: {}\n创建时间: {}(待处理)\n当前成员数: {}\n最大成员数: {}'.format(
+        group_info['group_id'], group_info['group_name'], group_info['admin_count'], admins_formatted,
+        group_info['create_time'], group_info['member_count'], group_info['max_member_count'])
+    return res
+
+
+# 获取所有群的QQ号
+async def get_group_numbers(bot: NoneBot):
+    return list(map(lambda x: x['group_id'], await bot.get_group_list()))
+
+
+# 获取所有好友的QQ号
+async def get_friend_numbers(bot: NoneBot):
+    group_list = await bot._get_friend_list()
+    res = []
+    id_group = list(map(lambda g: list(map(lambda i: i['user_id'], g['friends'])), group_list))
+    # 懒运算，不使用map的结果不会进行操作，因此使用list来强制进行操作
+    list(map(res.extend, id_group))
+    return res
